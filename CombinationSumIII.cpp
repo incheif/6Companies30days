cdf85d0d417dc14e6b2eba11f00aaee0ec6c0875
Dhruv Gupta <epost.dhruv@gmail.com>
2023-01-03 18:23:00 +0530

    first commit

---
+++ CombinationSumIII.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    void func(int k,int n, int index,vector<vector<int>> &ans,vector<int>&temp){
+        if(n==0 and k==0){
+            temp.erase(temp.begin());
+            ans.push_back(temp);
+            temp.insert(temp.begin(), 0);
+            return;
+        }
+        if(n<0){
+            return;
+        }
+        for(int i=index;i<=9;i++){
+
+            if(i!=temp.back()){
+                temp.push_back(i);
+                func(k-1,n-i,i+1,ans,temp);
+                temp.pop_back();
+            }
+        }
+        return;
+    }
+    vector<vector<int>> combinationSum3(int k, int n) {
+       vector<vector<int>>ans;
+       vector<int>temp;
+       temp.push_back(0);
+       func(k,n,1,ans,temp);
+        return ans;
+    }
+};
